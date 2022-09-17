eb0ae6c5eb20c02e57e00e6ef14362533d09b6bf
Iker Garcia <flipas.garcia@gmail.com>
2022-09-17 11:58:09 +0200

    test(math): ✅ add math tests

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,13 @@
+const { sum, multiply, divide } = require("./02-math");
+
+test("sum -> 1+3 should be 4", () => {
+  expect(sum(1, 3)).toBe(4);
+});
+
+test("multiply -> 1*3 should be 3", () => {
+  expect(multiply(1, 3)).toBe(4);
+});
+
+test("divide -> 1/3 should be 0", () => {
+  expect(divide(1, 3)).toBe(0.3333333333333333);
+});
